5285e7c0226057d0f1c7197e28b4126bf733997d
andreztz <andreztz@gmail.com>
2018-09-03 04:45:10 -0300

    Initial commit

---
+++ pyradios/radios.py
@@ -0,0 +1,151 @@
+import re
+import requests
+
+from xml.etree import ElementTree
+
+
+_base = "http://www.radio-browser.info/webservice/"
+
+
+def request(url, **kwargs):
+    # https://stackoverflow.com/questions/18308529/python-requests-package-handling-xml-response
+    formats = kwargs.get("formats")
+    resp = requests.get(url)
+    if resp.status_code == 200:
+        if formats == "xml":  # TODO
+            tree = ElementTree.fromstring(resp.content)
+            return tree.text
+        return resp.json()
+
+    return resp.raise_for_status()
+
+
+def build_url(endpoint=None, **kwargs):
+    formats = kwargs.get("formats", "json")
+    url = _base + formats + endpoint
+    return url
+
+
+class RadioBrowser:
+    def codecs(self, filters=None, **kwargs):
+        filters = kwargs.get("filters", filters)
+        endpoint = f"/codecs/{filters}" if filters else "/codecs"
+        url = build_url(endpoint, **kwargs)
+        return request(url, **kwargs)
+
+    def countries(self, filters=None, **kwrags):
+        filters = kwargs.get("filters", filters)
+        endpoint = f"/countries/{filters}" if filters else "/countries"
+        url = build_url(endpoint)
+        return request(url)
+
+    def languages(self, filters=None, **kwargs):
+        filters = kwargs.get("filters", filters)
+        endpoint = f"/languages/{filters}" if filters else "/languages"
+        url = build_url(endpoint)
+        return request(url)
+
+    def stations(self, searchterm=None, **kwargs):
+        # TODO implementar advanced search params = {}
+        endpoint = f"/stations"  # TODO ta faltando??
+        return request(url, **kwargs)  ## TODO ainda não faz request
+
+    def stations_byid(self, id, **kwargs):
+        id = kwargs.get("id")  # TODO testar se é string?
+        endpoint = f"/stations/byid/{id}"
+        url = build_url(endpoint)
+        return request(url)
+
+    def stations_byuuid(self, uuid, **kwargs):
+        name = kwargs.get("uuid")  # TODO testar se é string?
+        endpoint = f"/stations/byuuid/{uuid}"
+        url = build_url(endpoint)
+        return request(url)
+
+    def stations_byname(self, name, **kwargs):
+        name = kwargs.get("name")  # TODO testar se é string?
+        endpoint = f"/stations/byname/{name}"
+        url = build_url(endpoint)
+        return request(url)
+
+    def stations_bynameexact(self, name, **kwargs):
+        name = kwargs.get("name")  # TODO testar se é string?
+        endpoint = f"/stations/bynameexact/{name}"
+        url = build_url(endpoint)
+        return request(url)
+
+    def stations_bycodec(self, name, **kwargs):
+        name = kwargs.get("name")  # TODO testar se é string?
+        endpoint = f"/stations/bycodec/{name}"
+        url = build_url(endpoint)
+        return request(url)
+
+    def stations_bycodecexact(self, name, **kwargs):
+        name = kwargs.get("name")  # TODO testar se é string?
+        endpoint = f"/stations/bycodecexact/{name}"
+        url = build_url(endpoint)
+        return request(url)
+
+    def stations_bycountry(self, name, **kwargs):
+        name = kwargs.get("name")  # TODO testar se é string?
+        endpoint = f"/stations/bycountry/{name}"
+        url = build_url(endpoint)
+        return request(url)
+
+    def stations_bycountryexact(self, name, **kwargs):
+        name = kwargs.get("name")  # TODO testar se é string?
+        endpoint = f"/stations/bycountryexact/{name}"
+        url = build_url(endpoint)
+        return request(url)
+
+    def stations_bystateexact(self, name, **kwargs):
+        name = kwargs.get("name")  # TODO testar se é string?
+        endpoint = f"/stations/bystateexact/{name}"
+        url = build_url(endpoint)
+        return request(url)
+
+    def stations_bylanguage(self, name, **kwargs):
+        name = kwargs.get("name")  # TODO testar se é string?
+        endpoint = f"/stations/bylanguage/{name}"
+        url = build_url(endpoint)
+        return request(url)
+
+    def stations_bylanguageexact(self, name, **kwargs):
+        name = kwargs.get("name")  # TODO testar se é string?
+        endpoint = f"/stations/bylanguageexact/{name}"
+        url = build_url(endpoint)
+        return request(url)
+
+    def stations_bytag(self, name, **kwargs):
+        name = kwargs.get("name")  # TODO testar se é string?
+        endpoint = f"/stations/bytag/{name}"
+        url = build_url(endpoint)
+        return request(url)
+
+    def stations_bytagexact(self, name, **kwargs):
+        name = kwargs.get("name")  # TODO testar se é string?
+        endpoint = f"/stations/bytagexact/{name}"
+        url = build_url(endpoint)
+        return request(url)
+
+    def tags(self, filters=None, **kwargs):
+        filters = kwargs.get("filters", filters)
+        endpoint = f"/tags/{filters}" if filters else "/tags"
+        url = build_url(endpoint)
+        return request(url)
+
+    def playable_station(self, stationid, formats="json", **kwargs):
+        stationid = kwargs.get("stationid")
+        formats = kwargs.get("formats", formats)
+        endpoint = f"/{formats}/url/{stationid}"
+        url = build_url(endpoint)
+        return request(url)
+
+
+def main():
+    rb = RadioBrowser()
+    print(rb.stations_byname("DNB Radio Brazil"))
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests.py
@@ -0,0 +1,70 @@
+import unittest
+
+from pyradios.radios import RadioBrowser
+
+
+class TestRadioBrowser(unittest.TestCase):
+    def setUp(self):
+        self.rb = RadioBrowser()
+
+    def test_station_byname_response(self):
+        station = [
+            {
+                "id": "113234",
+                "changeuuid": "26b48bb1-a490-11e8-abb8-52543be04c81",
+                "stationuuid": "26b48b9d-a490-11e8-abb8-52543be04c81",
+                "name": "DNB Radio Brazil",
+                "url": "http://stm43.srvstm.com:21376/;",
+                "homepage": "https://dnbradio.com.br/",
+                "favicon": "https://dnbradio.com.br/wp/wp-content/uploads/2018/08/Sem-T%C3%ADtulo-3.png",
+                "tags": "drum and bass",
+                "country": "Brazil",
+                "state": "",
+                "language": "",
+                "votes": "10",
+                "negativevotes": "0",
+                "lastchangetime": "2018-08-20 17:46:07",
+                "ip": "78.18.215.141",
+                "codec": "AAC+",
+                "bitrate": "48",
+                "hls": "0",
+                "lastcheckok": "1",
+                "lastchecktime": "2018-09-02 17:48:04",
+                "lastcheckoktime": "2018-09-02 17:48:04",
+                "clicktimestamp": "2018-09-02 07:34:25",
+                "clickcount": "0",
+                "clicktrend": "0",
+            }
+        ]
+        expected_keys = station[0].keys()
+        self.assertEqual(self.rb.stations_byname("DNB Radio Brazil")[0].keys(), expected_keys)
+
+    def test_codecs_response(self):
+        codecs = [
+            {"name": "AAC", "value": "AAC", "stationcount": "624"},
+            {"name": "AAC+", "value": "AAC+", "stationcount": "4062"},
+            {"name": "AAC,H.264", "value": "AAC,H.264", "stationcount": "64"},
+            {"name": "FLV", "value": "FLV", "stationcount": "3"},
+            {"name": "MP3", "value": "MP3", "stationcount": "13792"},
+            {"name": "MP3,H.264", "value": "MP3,H.264", "stationcount": "2"},
+            {"name": "OGG", "value": "OGG", "stationcount": "211"},
+            {"name": "UNKNOWN", "value": "UNKNOWN", "stationcount": "670"},
+        ]
+        expected_keys = codecs[0].keys()
+        self.assertEqual(self.rb.codecs()[0].keys(), expected_keys)
+
+    def test_codecs_response_with_filters(self):
+        # filter example
+        # http://www.radio-browser.info/webservice/json/codecs/aac
+        # http://www.radio-browser.info/webservice/json/codecs/?reverse=true&hidebroken=true&order=stationcount
+        codecs_aac = [
+            {"name": "AAC", "value": "AAC", "stationcount": "624"},
+            {"name": "AAC+", "value": "AAC+", "stationcount": "4063"},
+            {"name": "AAC,H.264", "value": "AAC,H.264", "stationcount": "64"},
+        ]
+        expected_keys = codecs_aac[0].keys()
+        self.assertEqual(self.rb.codecs(filters="aac")[0].keys(), expected_keys)
+
+
+if __name__ == "__main__":
+    unittest.main()
