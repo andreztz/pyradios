9d017ebc8589c17805b780dec5260ee217c3b390
andreztz <andreztz@gmail.com>
2019-06-13 00:46:49 -0300

    Fix import.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,10 +1,9 @@
 import unittest
 
-from tests.api import TestEndPoint
+from tests.endpoint_builder import TestEndPoint
 
 
 def load_suit():
     loader = unittest.TestLoader()
     suite = loader.loadTestsFromTestCase(TestEndPoint)
     return suite
-
