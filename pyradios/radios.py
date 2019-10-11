02e55c2d9578beb06e7a482092355adbbf6caea1
André P. Santos <andreztz@gmail.com>
2019-10-11 20:02:01 -0300

    Changes default time for cache expiration.

--- pyradios/radios.py
+++ pyradios/radios.py
@@ -11,7 +11,7 @@ def request(endpoint, **kwargs):
 
     BASE_URL = pick_url(
         filename="data.cache",
-        expire=kwargs.get("expire", 60000),
+        expire=kwargs.get("expire", 604800),
         appname=kwargs.get("appname", "pyradios"),
         appauthor=kwargs.get("appname", "andreztz"),
     )
