83a6df21e32d50b6f48f036c9032c5711a5c5270
André <andreztz@gmail.com>
2020-02-07 14:08:14 -0300

    Change version - 0.0.20

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 from setuptools import setup
 from setuptools import find_packages
-from setuptools.command.develop import develop
-from setuptools.command.develop import develop as DevelopCommand
-from setuptools.command.test import test as TestCommand
-
 
 
 DESCRIPTION = (
@@ -21,38 +17,9 @@ def required():
         return f.read().splitlines()
 
 
-# import pip
-# dev_requirements = []
-
-
-# class CustomDevelopCommand(DevelopCommand):
-#     def run(self):
-#         super().run()
-#         pip.main(["install"] + dev_requirements)
-
-
-class CustomTestCommand(TestCommand):
-    # https://pytest.readthedocs.io/en/2.7.3/goodpractises.html#integrating-with-distutils-python-setup-py-test
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        #import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
-
 setup(
     name="pyradios",
-    version="0.0.18",
+    version="0.0.20",
     description=DESCRIPTION,
     long_description=readme(),
     long_description_content_type="text/markdown",
@@ -63,7 +30,6 @@ setup(
     license="MIT",
     packages=find_packages(),
     install_requires=required(),
-    tests_require=['pytest'],
     classifiers=[
         "Development Status :: 1 - Planning",
         "Environment :: Console",
@@ -71,5 +37,4 @@ setup(
         "Operating System :: OS Independent",
         "Intended Audience :: Developers",
     ],
-    cmdclass={"test": CustomTestCommand},
 )
