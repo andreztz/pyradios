d2e5f05abf1a7a60ef31ea5025f5e2d642df09be
André <andreztz@gmail.com>
2020-02-12 18:40:48 -0300

    Move bool2string to utils.

--- pyradios/utils.py
+++ pyradios/utils.py
@@ -15,3 +15,21 @@ def setup_log_file(filename, **kwargs):
 
 def setup_cache_file(filename, **kwargs):
     return create_app_dirs(filename, app_dirs.user_cache_dir)
+
+
+def bool2string(b):
+    """Convert a boolean type to string.
+
+    Args:
+        b (bool): A Boolean.
+
+    Raises:
+        ValueError: [description]
+
+    Returns:
+        str: String representation of a bool type.
+    """
+    s = str(b).lower()
+    if s in ["true", "false"]:
+        return s
+    raise TypeError("Value must be True or False.")
