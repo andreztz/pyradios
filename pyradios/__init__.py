a81b3ae90e11fbc86e2b9f1f599a979797847cca
André P. Santos <andreztz@gmail.com>
2020-11-28 03:41:46 -0300

    Add library-level logging capabilities

--- pyradios/__init__.py
+++ pyradios/__init__.py
@@ -1,5 +1,9 @@
+import logging
+from logging import NullHandler
 from pyradios.radios import RadioBrowser
 
-__version__ = '0.0.22'
+__version__ = "0.0.22"
 
-__all__ = ['RadioBrowser']
+__all__ = ["RadioBrowser"]
+
+logging.getLogger(__name__).addHandler(NullHandler())
