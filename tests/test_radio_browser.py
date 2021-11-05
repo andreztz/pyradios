cf96a1322afd70e8e2cfce30fc201b3394cc2d30
Marc Portier <marc.portier@gmail.com>
2021-11-05 11:31:39 +0100

    make tests work again
    - tag queries are case-sensitive + match parts of tags (tokens)
    - the hardcoded station used previously is not available any more
    - testing should not break if API adds new keys
    
    fixes #31

--- tests/test_radio_browser.py
+++ tests/test_radio_browser.py
@@ -1,5 +1,7 @@
 import pytest
 import responses
+import random
+import re
 
 from pyradios import RadioBrowser
 
@@ -26,6 +28,7 @@ def test_request_countrycodes(rb):
     )
 
     resp = rb.countrycodes()
+    assert len(resp) > 0, "at least one coountry should be in the response"
     assert "name" in resp[0]
     assert "stationcount" in resp[0]
     assert resp == expected
@@ -36,6 +39,7 @@ def test_request_countrycodes_with_filters(rb):
     # expected = [{"name": "BR", "stationcount": 607}]
 
     resp = rb.countrycodes(code="BR")
+    assert len(resp) > 0, "at least one coountry should be in the response"
     assert resp[0]["name"] == "BR"
 
 
@@ -44,6 +48,7 @@ def test_request_codecs(rb):
     # expected = [{"name": "MP3", "stationcount": 16620}]
 
     resp = rb.codecs()
+    assert len(resp) > 0, "at least one codec should be in the response"
     assert "name" in resp[0]
     assert "stationcount" in resp[0]
 
@@ -53,6 +58,7 @@ def test_request_codecs_with_filters(rb):
     # expected = [{"name": "MP3", "stationcount": 16620}]
 
     resp = rb.codecs(codec="mp3")
+    assert len(resp) > 0, "at least one codec should be in the response"
     assert resp[0]["name"] == "MP3"
 
 
@@ -62,6 +68,7 @@ def test_request_states_with_filters(rb):
 
     resp = rb.states(country="BRAZIL", state="Parana")
 
+    assert len(resp) > 0, "at least one state should be in the response"
     assert "name" in resp[0]
     assert "country" in resp[0]
     assert "stationcount" in resp[0]
@@ -76,6 +83,7 @@ def test_request_languages_with_filters(rb):
 
     resp = rb.languages(language="portuguese")
 
+    assert len(resp) > 0, "at least one language should be in the response"
     assert "name" in resp[0]
     assert "stationcount" in resp[0]
 
@@ -90,8 +98,9 @@ def test_request_tags_with_filters(rb):
     #     json=expected,
     #     status=200,
     # )
-    resp = rb.tags(tag="drum and bass")
+    resp = rb.tags(tag="Drum and Bass")
 
+    assert len(resp) > 0, "at least one tag should be in the response"
     assert "name" in resp[0]
     assert "stationcount" in resp[0]
 
@@ -124,40 +133,33 @@ def test_request_station_click_counter(rb):
     assert resp == expected
 
 
+def pick_random_station(rb, **params):
+    stations = rb.search(**params)
+    return random.choice(stations)
+
+
+def assert_expected_keys(station):
+    # Note: this assertion is more about the API service implmentation,
+    #       than it is about this py library
+    EXPECTED_STATION_KEYS = {
+        "changeuuid", "stationuuid", "name", "url",
+        "url_resolved", "homepage", "favicon", "tags", "country",
+        "countrycode", "state", "language", "votes", "lastchangetime",
+        "codec", "bitrate", "hls", "lastcheckok", "lastchecktime",
+        "lastcheckoktime", "lastlocalchecktime", "clicktimestamp",
+        "clickcount", "clicktrend",
+    }
+    missingkeys = EXPECTED_STATION_KEYS - set(station.keys())
+    assert len(missingkeys) == 0, "keys missing: %s" % missingkeys
+
+
 def test_request_station_byuuid(rb):
 
-    resp = rb.station_by_uuid("96062a7b-0601-11e8-ae97-52543be04c81")
+    uuid = pick_random_station(rb, limit=100)['stationuuid']
+    resp = rb.station_by_uuid(uuid)
 
-    super_set = {
-        "changeuuid",
-        "stationuuid",
-        "name",
-        "url",
-        "url_resolved",
-        "homepage",
-        "favicon",
-        "tags",
-        "country",
-        "countrycode",
-        "state",
-        "language",
-        "votes",
-        "lastchangetime",
-        "codec",
-        "bitrate",
-        "hls",
-        "lastcheckok",
-        "lastchecktime",
-        "lastcheckoktime",
-        "lastlocalchecktime",
-        "clicktimestamp",
-        "clickcount",
-        "clicktrend",
-    }
-    response_keys = resp[0].keys()
-
-    assert len(resp[0]) == len(response_keys)
-    assert super_set.issuperset(response_keys)
+    assert len(resp) == 1, "exactly one should match the uuid '%s'" % uuid
+    assert_expected_keys(resp[0])
 
 
 def test_request_station_tag_list(rb):
@@ -165,38 +167,18 @@ def test_request_station_tag_list(rb):
     tag_list = ["New York City", "Jazz"]
     resp = rb.stations_by_tag_list(tag_list)
 
-    super_set = {
-        "changeuuid",
-        "stationuuid",
-        "name",
-        "url",
-        "url_resolved",
-        "homepage",
-        "favicon",
-        "tags",
-        "country",
-        "countrycode",
-        "state",
-        "language",
-        "votes",
-        "lastchangetime",
-        "codec",
-        "bitrate",
-        "hls",
-        "lastcheckok",
-        "lastchecktime",
-        "lastcheckoktime",
-        "lastlocalchecktime",
-        "clicktimestamp",
-        "clickcount",
-        "clicktrend",
-    }
-    response_keys = resp[0].keys()
-    tags = set(resp[0]["tags"].split(","))
-    test_tag_list = [
-        tag.lower() for tag in tag_list
-    ]  # API is not case-sensitive
+    assert len(resp) > 0, "no match on tags [%s]" % ', '.join(tag_list)
 
-    assert len(resp[0]) == len(response_keys)
-    assert super_set.issuperset(response_keys)
-    assert tags.issuperset(test_tag_list)
+    some_stations = random.choices(resp, k=5)  # just take some stations
+    for station in some_stations:
+        assert_expected_keys(station)  # check it has the expected fields
+
+        # Note: this assertion is more about the API service implmentation,
+        #       than it is about this py library
+        # and be aware that tag search is token (or word) based
+        #        meaning that searching 'jazz' will match 'smooth jazz'
+        tagtokens = re.split(r"\s|,", station["tags"])
+        queried_tokens = re.split(r"\s|,", ','.join(tag_list).lower())
+        missing_tokens = set(queried_tokens) - set(tagtokens)
+        assert len(missing_tokens) == 0,\
+            "tokens missing %s in result-tag %s" % (missing_tokens, tagtokens)
