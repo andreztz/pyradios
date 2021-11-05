0a9c65915e0d0ae11b032dea09a1ae77796defc4
Marc Portier <marc.portier@gmail.com>
2021-11-05 17:35:17 +0100

    starting on the facets feature

---
+++ tests/test_radio_facets.py
@@ -0,0 +1,20 @@
+import pytest
+import responses
+import random
+import re
+
+from pyradios import RadioBrowser, RadioFacets
+
+
+BASE_URL = "https://nl1.api.radio-browser.info/"
+
+@pytest.fixture
+def rb():
+    _rb = RadioBrowser()
+    _rb.base_url = BASE_URL
+    return _rb
+
+def test_facet_init(rb):
+    rf = RadioFacets(rb)
+    assert rf.result is not None, "there should be a result-set"
+    assert len(rf.result) > 0, "the no-query result-set should not be empty"
