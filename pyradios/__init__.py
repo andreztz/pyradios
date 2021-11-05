94e894101be59b9f790910190fee751f2e0f7662
Marc Portier <marc.portier@gmail.com>
2021-11-05 17:39:54 +0100

    pleasing flake8

--- pyradios/__init__.py
+++ pyradios/__init__.py
@@ -6,6 +6,6 @@ from pyradios.facets import RadioFacets
 # warning: setup.py assumes strict single quotes on the line below
 __version__ = '0.0.22'
 
-__all__ = ["RadioBrowser"]
+__all__ = ["RadioBrowser", "RadioFacets"]
 
 logging.getLogger(__name__).addHandler(NullHandler())
