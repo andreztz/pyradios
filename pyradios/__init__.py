58058249757dd67b87caf12f508a32f2695598f8
Marc Portier <marc.portier@gmail.com>
2021-11-05 17:35:17 +0100

    starting on the facets feature

--- pyradios/__init__.py
+++ pyradios/__init__.py
@@ -1,6 +1,7 @@
 import logging
 from logging import NullHandler
 from pyradios.radios import RadioBrowser
+from pyradios.facets import RadioFacets
 
 # warning: setup.py assumes strict single quotes on the line below
 __version__ = '0.0.22'
