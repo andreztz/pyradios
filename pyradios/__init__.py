4bac573cdfa8385079d6b6814356fc54ea8f972e
Marc Portier <marc.portier@gmail.com>
2021-11-10 09:22:44 +0100

    making flake8 happy in order to have #33 pass the autiomated tests

--- pyradios/__init__.py
+++ pyradios/__init__.py
@@ -3,7 +3,7 @@ from logging import NullHandler
 from pyradios.radios import RadioBrowser
 
 # warning: setup.py assumes strict single quotes on the line below
-__version__ =  '0.0.22'
+__version__ = '0.0.22'
 
 __all__ = ["RadioBrowser"]
 
