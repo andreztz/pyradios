993a82c239040df0bb348afd985d45ea008dd317
Marc Portier <marc.portier@gmail.com>
2021-11-10 09:04:19 +0100

    introducing broader/narrow navigation from one set of facets to the next

--- tests/test_radio_facets.py
+++ tests/test_radio_facets.py
@@ -1,19 +1,79 @@
 import pytest
+import sys
+import logging
 
 from pyradios import RadioBrowser, RadioFacets
 
 
-BASE_URL = "https://nl1.api.radio-browser.info/"
+log = logging.getLogger("pyradios")
+
 
 
 @pytest.fixture
 def rb():
     _rb = RadioBrowser()
-    _rb.base_url = BASE_URL
     return _rb
 
 
-def test_facet_init(rb):
+def test_facet_init_and_narrow(rb):
+    log.debug("started")
     rf = RadioFacets(rb)
-    assert rf.result is not None, "there should be a result-set"
-    assert len(rf) > 0, "the no-query result-set should not be empty"
+    assert rf.result is not None, "expecting to have a result-set"
+    assert len(rf) > 0, "expecting the no-query result-set to not be empty"
+
+
+def test_facet_narrow_broaden(rb):
+    log.debug("started")
+    limit = 10
+    qry_be = dict(countrycode="be")  # the center of the world
+    qry_nl = dict(language="dutch")  # spoken in be as is french and german
+    qry_lim = dict(limit=limit)    # size limit
+    qry_klara = dict(name="klara", name_exact=False)  # radiostation in be
+    qry_class = dict(tag="classical")
+
+    # start off with a set of belgian stations
+    rfbe = RadioFacets(rb, **qry_be)
+    assert len(rfbe) > limit, f"execpting > {limit} stations in .be"
+    log.debug(f"found {len(rfbe)} stations matching {rfbe.filter}")
+
+    # narrow down to dutch speaking stations
+    rfbenl = rfbe.narrow(**qry_nl)
+    assert len(rfbenl) > limit, f"execpting > {limit} nl stations in .be"
+    assert len(rfbenl) < len(rfbe), f"expecting set to be < {len(rfbe)}"
+    log.debug(f"found {len(rfbenl)} stations matching {rfbenl.filter}")
+
+    # add the size limit
+    rfbenllim = rfbenl.narrow(**qry_lim)
+    assert len(rfbenllim) == limit, f"expecting set to be size {limit}"
+    log.debug(f"found {len(rfbenllim)} stations matching {rfbenllim.filter}")
+
+    # further narrow to name-matches
+    rfbenlklaralim = rfbenllim.narrow(**qry_klara)
+    assert 0 < len(rfbenlklaralim) < limit, f"expecting size between 0 < {limit}"
+    log.debug(f"found {len(rfbenlklaralim)} stations " +
+        f"matching {rfbenlklaralim.filter}")
+
+    rfklaralim = rfbenlklaralim.broaden(tuple({ **qry_be, **qry_nl}.keys()))
+    assert len(rfbenlklaralim) <= len(rfklaralim) < limit
+    log.debug(f"found {len(rfklaralim)} stations " +
+        f"matching {rfklaralim.filter}")
+
+
+
+def enable_stdout_logging():
+    log.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    log.addHandler(handler)
+
+
+if __name__ == "__main__":
+    enable_stdout_logging()
+    print(
+        "Running tests in ",
+        __file__,
+        "with -v(erbose) and -s(no stdout capturing) ",
+        "and DEBUG logging")
+    sys.exit(pytest.main(["-v", "-s",  __file__]))
