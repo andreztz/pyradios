9e145e7496915a07ba366a6e70dea5454c07f3c2
Marc Portier <marc.portier@gmail.com>
2021-11-10 09:37:35 +0100

    please flake8 with some cleanup

--- pyradios/facets.py
+++ pyradios/facets.py
@@ -32,11 +32,12 @@ class RadioFacets:
             filter.pop(k, None)
         return self._derived(**filter)
 
-    # todo implement ideas expressed in https://github.com/andreztz/pyradios/issues/32
+    # todo various ideas in https://github.com/andreztz/pyradios/issues/32
     #     fb = rb.facets(**query)
     #     fb.result       # yielding the equivalent of rb.search(**kwargs)
     #     fb.filter       # remembering the **kwargs used to filter
-    #     fb.tags         # yielding a similar list({"name": "..", "stationcount": ".."}) as rb.tags() but narrowed to the tags still in this resultset
-    #     fb.coutries     #  similarly so, as well as for languages, codecs, tags, ...
-    #     fb2 = fb.add(tag="extra filter tag", countrycode="be")    # an more constrained similar facets object
-    #     fb3 = fb.remove("tag", "language")                        # a less constrained one, removing he passed filter-items
+    #     fb.tags         # yielding a similar
+    #                     # list({"name": "..", "stationcount": ".."})
+    #                     # as rb.tags()
+    #                     # but narrowed to the tags still in this resultset
+    #     fb.coutries     # -- and idem for languages, codecs, ...
