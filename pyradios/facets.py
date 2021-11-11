da98fa3737afd8b1c8f6859e66295169e256a847
Marc Portier <marc.portier@gmail.com>
2021-11-11 11:23:51 +0100

    refactoring and cleanup

--- pyradios/facets.py
+++ pyradios/facets.py
@@ -1,4 +1,9 @@
 from pyradios.radios import RadioBrowser
+from itertools import chain
+import logging
+
+
+log = logging.getLogger("pyradios")
 
 
 class RadioFacets:
@@ -8,10 +13,15 @@ class RadioFacets:
         assert rb is not None, "facets requires a RadioBrowser service to call"
         assert isinstance(rb, RadioBrowser), "RadioBrowser service wrong type"
         self.rb = rb
-        self.filter = params
-        self._exec()
+        self.filter = params if params is not None else dict()
+        self._fetch_and_calc()
 
-    def _exec(self):
+    def __repr__(self):
+        typename = type(self).__name__
+        filter_repr = ['{}={!r}'.format(k, v) for k, v in self.filter.items()]
+        return "%s(%s)" % (typename, ', '.join(filter_repr))
+
+    def _fetch_and_calc(self):
         self.result = self.rb.search(**self.filter)
         # todo - calculate counts and histograms on everything
 
@@ -21,14 +31,16 @@ class RadioFacets:
     def _derived(self, **filter):
         return RadioFacets(self.rb, **filter)
 
-    def narrow(self, **extra_params):
-        filter = dict(self.filter)
-        filter.update(extra_params)
+    def narrow(self, **pars2add):
+        log.debug("flatten ++ params(%s)" % pars2add)
+        filter = dict(self.filter)  # clone the dict
+        filter.update(pars2add)
         return self._derived(**filter)
 
-    def broaden(self, *keys_to_remove):
+    def broaden(self, *keys2rm, **pars2rm):
+        log.debug("broaden -- keys(%s) and params(%s)" % (keys2rm, pars2rm))
         filter = dict(self.filter)
-        for k in keys_to_remove:
+        for k in chain(keys2rm, pars2rm.keys()):
             filter.pop(k, None)
         return self._derived(**filter)
 
