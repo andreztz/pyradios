b3d968199f6753c50da9bdead2a14ab276ac39b6
André P. Santos <andreztz@gmail.com>
2021-11-15 22:06:21 -0300

    Add test for the station structure
    Checks if the keys in the API response structure have changed.

--- tests/test_radio_browser.py
+++ tests/test_radio_browser.py
@@ -1,7 +1,8 @@
-import pytest
-import responses
-import random
 import re
+
+import pytest
+import random
+import responses
 
 from pyradios import RadioBrowser
 from pyradios.radios import version
@@ -10,8 +11,13 @@ from pyradios.radios import version
 BASE_URL = "https://nl1.api.radio-browser.info/"
 
 
+def pick_random_station(rb, **params):
+    stations = rb.search(**params)
+    return random.choice(stations)
+
+
 def test_version():
-    assert version == '0.0.22'
+    assert version == "0.0.22"
 
 
 @pytest.fixture
@@ -19,6 +25,94 @@ def rb():
     _rb = RadioBrowser()
     _rb.base_url = BASE_URL
     return _rb
+
+
+@pytest.fixture
+def station_struct():
+    """https://de1.api.radio-browser.info/#Struct_station"""
+    return {
+        "changeuuid",
+        "stationuuid",
+        "name",
+        "url",
+        "url_resolved",
+        "homepage",
+        "favicon",
+        "tags",
+        "country",
+        "countrycode",
+        "state",
+        "language",
+        "languagecodes",
+        "votes",
+        "lastchangetime",
+        "lastchangetime_iso8601",
+        "codec",
+        "bitrate",
+        "hls",
+        "lastcheckok",
+        "lastchecktime",
+        "lastchecktime_iso8601",
+        "lastcheckoktime",
+        "lastcheckoktime_iso8601",
+        "lastlocalchecktime",
+        "lastlocalchecktime_iso8601",
+        "clicktimestamp",
+        "clicktimestamp_iso8601",
+        "clickcount",
+        "clicktrend",
+        "ssl_error",
+        "geo_lat",
+        "geo_long",
+        "serveruuid",
+        "has_extended_info",
+        "iso_3166_2",
+    }
+
+
+def test_station_struct(rb, station_struct):
+    """
+    Checks if the keys in the API response structure have changed.
+
+    Note: This test is more about the API service implmentation, than
+    it is about this python library.
+
+    See details:
+        https://de1.api.radio-browser.info/#Struct_station
+    """
+    stationuuid = "d97b5842-8e9b-46cc-85f0-d2dff6738c7c"
+    resp = rb.station_by_uuid(stationuuid)
+    response_keys = set(resp[0].keys())
+    missing_keys = response_keys.difference(station_struct)
+    assert len(missing_keys) == 0, f"Keys missing {missing_keys}"
+
+
+@responses.activate
+def test_request_station_click_counter(rb):
+
+    expected = {
+        "ok": True,
+        "message": "retrieved station url",
+        "stationuuid": "96062a7b-0601-11e8-ae97-52543be04c81",
+        "name": "BBC Radio 1",
+        "url": "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1_mf_p",
+    }
+    # mock responses
+    responses.add(
+        responses.GET,
+        BASE_URL + "json/url/96062a7b-0601-11e8-ae97-52543be04c81",
+        json=expected,
+        status=200,
+    )
+
+    resp = rb.click_counter("96062a7b-0601-11e8-ae97-52543be04c81")
+
+    assert "ok" in resp
+    assert "message" in resp
+    assert "stationuuid" in resp
+    assert "name" in resp
+    assert "url" in resp
+    assert resp == expected
 
 
 @responses.activate
@@ -129,61 +223,12 @@ def test_request_tags_with_filters(rb):
     assert "stationcount" in resp[0]
 
 
-@responses.activate
-def test_request_station_click_counter(rb):
-
-    expected = {
-        "ok": True,
-        "message": "retrieved station url",
-        "stationuuid": "96062a7b-0601-11e8-ae97-52543be04c81",
-        "name": "BBC Radio 1",
-        "url": "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1_mf_p",
-    }
-    # mock responses
-    responses.add(
-        responses.GET,
-        BASE_URL + "json/url/96062a7b-0601-11e8-ae97-52543be04c81",
-        json=expected,
-        status=200,
-    )
-
-    resp = rb.click_counter("96062a7b-0601-11e8-ae97-52543be04c81")
-
-    assert "ok" in resp
-    assert "message" in resp
-    assert "stationuuid" in resp
-    assert "name" in resp
-    assert "url" in resp
-    assert resp == expected
-
-
-def pick_random_station(rb, **params):
-    stations = rb.search(**params)
-    return random.choice(stations)
-
-
-def assert_expected_keys(station):
-    # Note: this assertion is more about the API service implmentation,
-    #       than it is about this py library
-    expected_station_keys = {
-        "changeuuid", "stationuuid", "name", "url",
-        "url_resolved", "homepage", "favicon", "tags", "country",
-        "countrycode", "state", "language", "votes", "lastchangetime",
-        "codec", "bitrate", "hls", "lastcheckok", "lastchecktime",
-        "lastcheckoktime", "lastlocalchecktime", "clicktimestamp",
-        "clickcount", "clicktrend",
-    }
-    missingkeys = expected_station_keys - set(station.keys())
-    assert len(missingkeys) == 0, "keys missing: %s" % missingkeys
-
-
 def test_request_station_byuuid(rb):
 
-    uuid = pick_random_station(rb, limit=100)['stationuuid']
+    uuid = pick_random_station(rb, limit=100)["stationuuid"]
     resp = rb.station_by_uuid(uuid)
 
     assert len(resp) == 1, "exactly one should match the uuid '%s'" % uuid
-    assert_expected_keys(resp[0])
 
 
 def test_request_station_tag_list(rb):
@@ -191,18 +236,17 @@ def test_request_station_tag_list(rb):
     tag_list = ["New York City", "Jazz"]
     resp = rb.stations_by_tag_list(tag_list)
 
-    assert len(resp) > 0, "no match on tags [%s]" % ', '.join(tag_list)
+    assert len(resp) > 0, "no match on tags [%s]" % ", ".join(tag_list)
 
     some_stations = random.choices(resp, k=5)  # just take some stations
-    for station in some_stations:
-        assert_expected_keys(station)  # check it has the expected fields
 
-        # Note: this assertion is more about the API service implmentation,
-        #       than it is about this py library
-        # and be aware that tag search is token (or word) based
-        #        meaning that searching 'jazz' will match 'smooth jazz'
+    for station in some_stations:
+
+        # Be aware that tag search is token (or word) based
+        # meaning that searching 'jazz' will match 'smooth jazz'
         tagtokens = re.split(r"\s|,", station["tags"])
-        queried_tokens = re.split(r"\s|,", ','.join(tag_list).lower())
+        queried_tokens = re.split(r"\s|,", ",".join(tag_list).lower())
         missing_tokens = set(queried_tokens) - set(tagtokens)
-        assert len(missing_tokens) == 0,\
-            "tokens missing %s in result-tag %s" % (missing_tokens, tagtokens)
+        assert (
+            len(missing_tokens) == 0
+        ), "tokens missing %s in result-tag %s" % (missing_tokens, tagtokens)
