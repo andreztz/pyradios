6d0e6c4cdd2525f5eb83bf96f5fdd2896e45e23a
André P. Santos <andreztz@gmail.com>
2021-11-24 14:11:51 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 
-DESCRIPTION = "Python client for the Radio Browser API"
+DESCRIPTION = "Python client for Radio Browser API"
 
 
 def get_version():
