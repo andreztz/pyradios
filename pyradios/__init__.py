5053ab1f95725f4b3ed89aab37abfa1b4e1bdff1
André P. Santos <andreztz@gmail.com>
2023-12-12 10:24:01 -0300

    Migrate HTTP client library to httpx
    Changes:
    
    - Replaced `requests` with `httpx` as the default HTTP client.
    - Update existing cassette recordings with new data.
    - Added `pytest-mock` for mocking dependencies in tests.
    - Remove outdated dependency.
    - Bumped the version number to 2.1.0.
    
    Reason:
    
    The switch from the default HTTP client library, `requests`, to `httpx` aims to
    modernize the project's underlying infrastructure, providing a more up-to-date
    and flexible foundation. This update also opens the possibility of implementing
    asynchronous functionalities in the future. It's a significant step to align
    the project with modern development practices and ensure greater flexibility
    for future expansions.

--- pyradios/__init__.py
+++ pyradios/__init__.py
@@ -4,7 +4,7 @@ from pyradios.radios import RadioBrowser
 from pyradios.facets import RadioFacets
 
 # warning: setup.py assumes strict single quotes on the line below
-__version__ = '2.0.0'
+__version__ = '2.1.0'
 
 __all__ = ["RadioBrowser", "RadioFacets"]
 
